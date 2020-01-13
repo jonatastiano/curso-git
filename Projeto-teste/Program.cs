cbd194f2380acc1aa8f5fc5ec25e725cad8b175d
Jonatas Tiano <jonatasepriscilatiano123@gmail.com>
2020-01-13 00:40:01 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
